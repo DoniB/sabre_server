3f92abefb07fd792f294d5f7c4d46f8aecf30c18
Donizety <dbaltokoski@gmail.com>
2018-09-29 23:18:45 -0300

    add user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  #validar email, é email, unico, salvando tudo em minusculo
+  #nome requerido
+  #senha requerido,  minimo 6 digitos
+  #password_confirmation requerido, minimo 6 digitos
+
+  #user.authenticate(password)
+end
