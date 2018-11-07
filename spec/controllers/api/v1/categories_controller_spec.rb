609fd318f42e69ac9ba6d3f639cf0435d88a7a8d
Donizety <dbaltokoski@gmail.com>
2018-11-07 01:42:12 -0200

    add categories controller

---
+++ spec/controllers/api/v1/categories_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::CategoriesController, type: :controller do
+
+  describe 'GET #index' do
+    it 'returns all categories' do
+      get :index
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(json.size).to eq(0)
+
+      category = create(:category)
+      get :index
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(json.size).to eq(1)
+      expect(json[0]['id']).to eq(category.id)
+      expect(json[0]['name']).to eq(category.name)
+
+      4.times { create(:category) }
+      get :index
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(json.size).to eq(5)
+    end
+  end
+
+end
