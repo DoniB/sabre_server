2475d568fb2b0be2af5a804a3670539ae20aa4d6
Donizety <dbaltokoski@gmail.com>
2019-03-14 17:45:05 -0300

    fix production settings

--- start_server.sh
+++ start_server.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+
+rm -rf /opt/sabre/tmp/pids/server.pi*
+bundle exec rake db:create
+bundle exec rake db:migrate
+
 if [ "$RAILS_ENV" == "production" ]
 then
-    bundle exec rake db:migrate
     echo Starting production server...
     bundle exec puma -C config/puma.rb
 else
-    rm -rf /opt/sabre/tmp/pids/server.pi*
-    bundle exec rake db:create
-    bundle exec rake db:migrate
     bundle exec rake db:seed
     echo Starting development server...
     bundle exec rails s -p 3000 -b 0.0.0.0
