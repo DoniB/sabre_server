7fff1ea6c7b51ed47f6de9d7e084327268c22b1a
Donizety <dbaltokoski@gmail.com>
2019-03-16 15:59:10 -0300

    fix database error

--- start_server.sh
+++ start_server.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Waiting for PostgreSQL to launch on 5432..."
-while ! nc -z localhost 5432; do
+while ! nc -z postgres 5432; do
   sleep 0.1
 done
 echo "PostgreSQL launched"
 
 rm -rf /opt/sabre/tmp/pids/server.pi*
-bundle exec rake db:create
+bundle exec rake db:create &> /dev/null && echo "Database created" || echo "Database already exists"
 bundle exec rake db:migrate
 
 if [ "$RAILS_ENV" == "production" ]
